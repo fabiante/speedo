96e420909a13c5b948748343975ab3123f4a147d
fabiante <fabiante@users.noreply.github.com>
2024-12-29 18:04:34 +0100

    Remove comments

--- main.go
+++ main.go
@@ -13,24 +13,6 @@ func main() {
 
 	var speedtestClient = speedtest.New()
 
-	// Use a proxy for the speedtest. eg: socks://127.0.0.1:7890
-	// speedtest.WithUserConfig(&speedtest.UserConfig{Proxy: "socks://127.0.0.1:7890"})(speedtestClient)
-
-	// Select a network card as the data interface.
-	// speedtest.WithUserConfig(&speedtest.UserConfig{Source: "192.168.1.101"})(speedtestClient)
-
-	// Get user's network information
-	// user, _ := speedtestClient.FetchUserInfo()
-
-	// Get a list of servers near a specified location
-	// user.SetLocationByCity("Tokyo")
-	// user.SetLocation("Osaka", 34.6952, 135.5006)
-
-	// Search server using serverID.
-	// eg: fetch server with ID 28910.
-	// speedtest.ErrServerNotFound will be returned if the server cannot be found.
-	// server, err := speedtest.FetchServerByID("28910")
-
 	serverList, _ := speedtestClient.FetchServers()
 	targets, _ := serverList.FindServer([]int{61387})
 
