c8137e799aebdc9e961877d55282cbb97d9702f8
fabiante <fabiante@users.noreply.github.com>
2024-12-29 12:12:30 +0100

    Add structured log output and specific servers

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+
 	"github.com/showwin/speedtest-go/speedtest"
 )
 
 func main() {
 	var speedtestClient = speedtest.New()
-	
+
 	// Use a proxy for the speedtest. eg: socks://127.0.0.1:7890
 	// speedtest.WithUserConfig(&speedtest.UserConfig{Proxy: "socks://127.0.0.1:7890"})(speedtestClient)
-	
+
 	// Select a network card as the data interface.
 	// speedtest.WithUserConfig(&speedtest.UserConfig{Source: "192.168.1.101"})(speedtestClient)
-	
+
 	// Get user's network information
 	// user, _ := speedtestClient.FetchUserInfo()
-	
+
 	// Get a list of servers near a specified location
 	// user.SetLocationByCity("Tokyo")
 	// user.SetLocation("Osaka", 34.6952, 135.5006)
-    
+
 	// Search server using serverID.
 	// eg: fetch server with ID 28910.
 	// speedtest.ErrServerNotFound will be returned if the server cannot be found.
 	// server, err := speedtest.FetchServerByID("28910")
-	
+
 	serverList, _ := speedtestClient.FetchServers()
-	targets, _ := serverList.FindServer([]int{})
+	targets, _ := serverList.FindServer([]int{61387, 5747, 44599, 4087, 8827})
 
 	for _, s := range targets {
 		// Please make sure your host can access this test server,
@@ -36,8 +37,7 @@ func main() {
 		s.PingTest(nil)
 		s.DownloadTest()
 		s.UploadTest()
-		// Note: The unit of s.DLSpeed, s.ULSpeed is bytes per second, this is a float64.
-		fmt.Printf("Latency: %s, Download: %s, Upload: %s\n", s.Latency, s.DLSpeed, s.ULSpeed)
+		slog.Info("Test done", "serverID", s.ID, "latency", s.Lat, "downloadMbps", s.DLSpeed.Mbps(), "uploadMbps", s.ULSpeed.Mbps())
 		s.Context.Reset() // reset counter
 	}
 }
