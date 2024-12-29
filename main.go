dee3078d4f1b2afdf7def14c21afadbeecd7eac3
fabiante <fabiante@users.noreply.github.com>
2024-12-29 18:10:46 +0100

    Refactor main for cmd pattern and add configurable log level

--- main.go
+++ main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 
 	"github.com/showwin/speedtest-go/speedtest"
 )
 
 func main() {
-	var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	args := os.Args[1:]
+
+	var cmd string
+
+	if len(args) == 0 {
+		cmd = "run"
+	} else {
+		cmd = args[0]
+		args = args[1:]
+	}
+
+	switch cmd {
+	case "run":
+		run()
+	default:
+		fmt.Println("Unknown command", "cmd", cmd)
+		os.Exit(1)
+	}
+}
+
+func run() {
+	logger := newLogger()
 
 	var speedtestClient = speedtest.New()
 
 	serverList, _ := speedtestClient.FetchServers()
 	targets, _ := serverList.FindServer([]int{61387})
+
+	logger.Debug("Running tests on servers", "servers", targets)
 
 	for _, s := range targets {
 		var errs []error
@@ -29,4 +54,27 @@ func main() {
 		logger.Info("Test done", "serverID", s.ID, "downloadMbps", s.DLSpeed.Mbps(), "uploadMbps", s.ULSpeed.Mbps())
 		s.Context.Reset() // reset counter
 	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func newLogger() *slog.Logger {
+	levels := map[string]slog.Level{
+		"debug": slog.LevelDebug,
+		"info":  slog.LevelInfo,
+		"warn":  slog.LevelWarn,
+		"error": slog.LevelError,
+	}
+
+	levelStr := strings.ToLower(getEnv("LOG_LEVEL", "info"))
+	level := levels[levelStr]
+
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: level,
+	}))
 }
